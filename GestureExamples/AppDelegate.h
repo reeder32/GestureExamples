f23010f8a09494b69b08a953fed2f8e448a9bb6e
reeder32 <nickreeder32@gmail.com>
2014-12-15 18:13:40 -0800

    Initial Commit

---
+++ GestureExamples/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GestureExamples
+//
+//  Created by Nick Reeder on 12/15/14.
+//  Copyright (c) 2014 Nick Reeder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GestureExamples/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GestureExamples
+//
+//  Created by Nick Reeder on 12/15/14.
+//  Copyright (c) 2014 Nick Reeder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
